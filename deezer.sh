16dbd894129849b6d4a6f6dc961725657a67d743
stonneau <4620966+stonneau@users.noreply.github.com>
2026-01-04 00:18:11 +0000

    run file, needs cleaning

---
+++ deezer.sh
@@ -0,0 +1,22 @@
+flatpak \
+  --env=DZ_START_IN_TRAY=no \
+  --env=DZ_HIDE_OFFLINE_BANNER=no \
+  run dev.aunetx.deezer \
+  --disable-gpu \
+  --disable-software-rasterizer \
+  --disable-dev-shm-usage \
+  --disable-logging \
+  --disable-breakpad \
+  --no-sandbox \
+  --disable-accelerated-video-decode \
+  --enable-features=OverlayScrollbar \
+  --disable-animations \
+  --disable-notifications \
+  --hide-offline-banner \
+  --force-device-scale-factor=1.6 \
+  --high-dpi-support=1 \
+  --enable-smooth-scrolling \ 
+  --disable-animations  \ 
+  --disable-features 
+
+
